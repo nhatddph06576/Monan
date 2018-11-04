312f4ef4a642e6dbbb82b48c296b67c02963ffe8
nhatddph06576 <nhatddph06576@fpt.edu.vn>
2018-11-04 17:37:51 +0700

    Initial commit

---
+++ app/src/main/java/nhat/duan1/HelloActivity.java
@@ -0,0 +1,27 @@
+package nhat.duan1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class HelloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(HelloActivity.this,HomeActivity.class));
+
+                //Tắt màn hình chào
+                finish();
+            }
+        },2000);
+    }
+}
